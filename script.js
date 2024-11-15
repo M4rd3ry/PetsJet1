def5bdc9d6cd0b83c3a8db301ee2018a3065cbf4
M4rd3ry <mardery77@gmail.com>
2024-11-15 22:50:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const coupon = document.getElementById('coupon');
     const closeBtn = document.getElementById('close-coupon');
 
-    setTimeout(() => {
-        coupon.classList.remove('hidden'); // Показывает купон через 10 секунд
-    }, 10000);
+    if (coupon && closeBtn) {
+        setTimeout(() => {
+            coupon.classList.remove('hidden'); // Показывает окно через 10 секунд
+        }, 10000);
 
-    closeBtn.addEventListener('click', () => {
-        coupon.classList.add('hidden'); // Закрывает купон по клику
-    });
+        closeBtn.addEventListener('click', () => {
+            coupon.classList.add('hidden'); // Закрывает окно при клике
+        });
+    } else {
+        console.error('Element not found: #coupon or #close-coupon');
+    }
 });
